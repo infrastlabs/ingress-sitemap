306d7f6f831da388a5c2699a9307847b90789f44
sam <huapox@126.com>
2020-06-26 11:41:53 +0800

    title, domain, port

--- ingress/ingsitemap.go
+++ ingress/ingsitemap.go
@@ -24,8 +24,8 @@ import (
 
 var (
     //read kubeconfig
-    kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
-	// kubeconfig = "/_ext/Development/Project/devcn.fun/g-dev2/fk-kubernetes-auto-ingress/kubeconfig-vm23.203"
+    // kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+	kubeconfig = "/_ext/Development/Project/devcn.fun/g-dev2/fk-kubernetes-auto-ingress/kubeconfig-vm23.203"
     clientset *kubernetes.Clientset
 )
 
@@ -35,8 +35,8 @@ func init(){
     var err error
     var config *rest.Config
 
-    if *kubeconfig != "" {
-        config, err = clientcmd.BuildConfigFromFlags("", *kubeconfig)
+    if kubeconfig != "" {
+        config, err = clientcmd.BuildConfigFromFlags("", kubeconfig)
     } else {
         //get config when running inside Kubernetes
         config, err = rest.InClusterConfig()
@@ -59,8 +59,9 @@ func init(){
 }
 
 type data struct {
-    Url   string
     Title string
+    Url   string
+    Port  string
 }
 type Show struct {
     Pages []*data
@@ -78,8 +79,8 @@ func GetIngs()([]*data) {
 		log.Info("get ingress: ", ing.Name, " > ", ing.Spec.Rules[0].Host)
 		d:= new(data)
 		d.Title=ing.Name
-		d.Url=ing.Spec.Rules[0].Host+ ":" + httpsPort  //+":31714"
-
+		d.Url=ing.Spec.Rules[0].Host //+ ":" + httpsPort  //+":31714"
+        d.Port=httpsPort
 		pages= append(pages, d)
 	}
 
